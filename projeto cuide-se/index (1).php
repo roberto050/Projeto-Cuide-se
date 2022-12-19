77fdc48911bbc008ceb3c934acdd405b22d598f0
roberto050 <104596522+roberto050@users.noreply.github.com>
2022-12-19 00:41:05 -0300

    Add files via upload

---
+++ projeto cuide-se/index (1).php
@@ -0,0 +1,241 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cuide-Se</title>
+  <link rel="icon" href="./images/pngwing.com.png">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./glightbox/css/glightbox.min.css">
+  <link rel="stylesheet" href="./swiper/swiper-bundle.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+  <section>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="./images/iconecuidese.png" alt="logo">
+          Cuide-Se
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#inicio">Início</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#menu">Categoria</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contato">Contato</a>
+            </li>
+          </ul>
+             <button class="b_button">Login</button>
+        </div>
+        <div>
+            <button class="c_button">Cadastrar-se</button>
+        </div>
+      </div>
+    </nav>
+  </section>
+  <div class="container-test">
+    
+  </div>
+  <section>
+    <div class="main-box" id="inicio">
+      <div class="title2">
+        <h1>Cuide<h2>se</h2>
+        </h1>
+        <p>O melhor para sua saúde</p>
+
+      </div>
+      <div class="thumbnail">
+        <img src="./images/testebanner.png" alt="banner">
+      </div>
+    </div>
+  </section>
+
+  <section id="menu" class="menu">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-header">
+        <h2>Categorias</h2>
+      </div>
+
+      <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
+
+
+        
+
+        <!-- <li class="nav-item"  active show>
+          <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-breakfast">
+            <h4>Sem Lactose</h4>
+        
+          </a> -->
+        <!-- End tab nav item -->
+
+        <!-- <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-lunch">
+            <h4>Sem Glúten</h4>
+          </a>
+        </li> -->
+        <!-- End tab nav item -->
+
+        <!-- <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-dinner">
+            <h4>Sem Açúcar</h4>
+          </a>
+         
+
+        </li> -->
+        <!-- End tab nav item -->
+
+      </ul>
+
+
+
+
+
+
+
+
+
+      <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+
+        <div class="tab-pane fade active show" id="results">
+
+          <div class="tab-header text-center">
+            
+          </div>
+
+          <div class="row gy-5">
+
+           
+          </div>
+     
+      <!--End Starter Menu Content-->
+
+
+
+
+
+      <!-- <div class="tab-pane fade" id="menu-breakfast">
+
+          <div class="tab-header text-center">
+            <h3>Sem Lactose</h3>
+          </div>
+
+          <div class="row gy-5" id = "row2">
+
+            
+          </div>
+        </div> -->
+      <!--End Breakfast Menu Content-->
+
+      <!-- <div class="tab-pane fade" id="menu-lunch">
+
+          <div class="tab-header text-center">
+            <h3>Sem Glúten</h3>
+          </div>
+
+          <div class="row gy-5" id = "row3">
+
+            
+          </div>
+        </div> -->
+      <!--End Lunch Menu Content-->
+
+      <!-- <div class="tab-pane fade" id="menu-dinner">
+
+          <div class="tab-header text-center">
+            <h3>Sem Açúcar</h3>
+          </div>
+
+          <div class="row gy-5" id = "row4">
+
+            
+          </div>
+        </div> -->
+      <!--End Dinner Menu Content-->
+
+        </div>
+
+      </div>
+  </section>
+
+
+  <footer class="footer section bd-container" id="contato">
+    <div class="footer_container bd-grid">
+      <div class="section-header-contato">
+        <h2>Contatos</h2>
+      </div>
+
+      <div class="footer_content">
+        <b class="footer_logo">
+          <span>Cuide-se</span>
+        </b>
+
+        <span class="footer_description">O melhor serviço, para você. </span>
+
+        <div>
+          <a href="#" class="footer_social"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="footer_social"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="footer_social"><i class="bx bxl-twitter"></i></a>
+        </div>
+      </div>
+
+      <div class="footer_content">
+        <h3 class="footer_title">Informações</h3>
+        <ul>
+          <li><a href="#" class="footer_link">Eventos</a></li>
+          <li><a href="#" class="footer_link">Política de Privacidade</a></li>
+          <li><a href="#" class="footer_link">Termos de serviço</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_content">
+        <h3 class="footer_title">Endereço</h3>
+        <ul>
+          <li><a href="https://goo.gl/maps/pYxuvHELg2oAoNY7A">Feira de Santana - BA</a></li>
+        </ul>
+      </div>
+
+      <div class="footer_content">
+        <h3 class="footer_title">Navegação</h3>
+        <ul>
+          <li><a href="#app" class="footer_link">Download</a></li>
+          <li><a href="#contact" class="footer_link">Contate-nos</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <p class="footer_copy">
+      &#169; 2022, Cuide-se
+      <a href="https://github.com/Thiagoow/UberEats-Responsive-LandingPage" class="footer_link" target="_blank">
+      </a>
+    </p>
+  </footer>
+  </div>
+
+
+
+  <script src="./glightbox/js/glightbox.min.js"></script>
+  <script src="./swiper/swiper-bundle.min.js"></script>
+
+  <script src="./js/first.js"></script>
+  <script src="./js/main.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
